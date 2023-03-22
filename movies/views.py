67868dde81f25f612973b31217d579d0574df676
Danil <danchic1556@gmail.com>
2023-03-22 10:41:39 +0300

    Add static

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+from .models import Movie
+
+
+class MoviesView(View):
+    """Список постов"""
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movies/movies.html', {'movies_list': movies})
