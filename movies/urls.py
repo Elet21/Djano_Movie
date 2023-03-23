f8b8b7ef34400abf96e45fb63395dc8ab08c781b
Danil <danchic1556@gmail.com>
2023-03-23 10:54:27 +0300

    Add movie_detail

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MoviesView
+from .views import MoviesView, DetailView
 
 urlpatterns = [
-    path('', MoviesView.as_view())
+    path('', MoviesView.as_view()),
+    path('<slug:slug>', DetailView.as_view(), name='movie_detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -11,3 +11,10 @@ class MoviesView(View):
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, 'movies/movies.html', {'movies_list': movies})
+
+
+class DetailView(View):
+    """Описание фильма"""
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, 'movies/movie_detail.html', {'movie': movie})
